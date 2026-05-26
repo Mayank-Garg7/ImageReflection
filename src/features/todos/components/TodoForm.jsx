a0e7399f05663e18802c087cf2379acae172e478
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-05-26 21:34:49 +0530

    take input form user

--- src/features/todos/components/TodoForm.jsx
+++ src/features/todos/components/TodoForm.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-
+import React, { useState, useContext } from "react";
+import TodoContext from "../context/TodoContext";
 
 function TodoForm() {
+  const [text, setText] = useState("");
+
+  const { task, setTask } = useContext(TodoContext);
+
+ 
+
   return (
-    <div>
-      
-    </div>
-  )
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter task"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+
+      <button type="submit">
+        Add Task
+      </button>
+    </form>
+  );
 }
 
-export default TodoForm
+export default TodoForm;
