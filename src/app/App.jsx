f46dd0362e5fff46eea364ca5c40aeb832e4ff0f
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-05-26 18:57:11 +0530

    fix: fix the routes here

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ContextProvider } from '../features/todos/context/TodoContext'
-import TodoForm from '../../../features/todos/components/TodoForm'
+import TodoForm from '../features/todos/components/TodoForm'
 import ShowTodos from '../../../features/todos/components/ShowTodos'
 
 function App() {
