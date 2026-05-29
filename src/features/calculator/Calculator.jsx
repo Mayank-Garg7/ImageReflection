4d2d97b5612b00ba56aa7faa8e25c8eea88792cf
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-05-29 20:17:32 +0530

    concise the code to more cleaner and understable

--- src/features/calculator/Calculator.jsx
+++ src/features/calculator/Calculator.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import Button from '../../shared/Button'
+import Card from '../../shared/Card'
 
 function Calculator() {
-    const arr = [0,1,2,3,4,5,6,7,8,9,"+,-,%,/,+,ac,x"]
-  return (
-    <div>
-      {arr.map((data, idx)=> (
-        <Button key= {idx}>
-            <span>{data}</span>
-        </Button>
-      ))}
-    </div>
-  )
+    const arr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "%", "/", "+", "ac", "x"];
+    return (
+        <Card>
+            <form >
+                <input type="text" placeholder='calculate...'/>
+                {arr.map((data, idx) => (
+                    <Button key={idx} variant='secondary'>
+                        <span>{data}</span>
+                    </Button>
+                ))}
+            </form>
+        </Card>
+    )
 }
 
 export default Calculator
