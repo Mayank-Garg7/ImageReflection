e698af05369995539629ef3a08a97fb4720af909
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-05-29 20:28:44 +0530

    chore: update the todos form

--- src/features/calculator/Calculator.jsx
+++ src/features/calculator/Calculator.jsx
@@ -6,13 +6,15 @@ function Calculator() {
     const arr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "%", "/", "+", "ac", "x"];
     return (
         <Card>
-            <form >
-                <input type="text" placeholder='calculate...'/>
-                {arr.map((data, idx) => (
-                    <Button key={idx} variant='secondary'>
-                        <span>{data}</span>
-                    </Button>
-                ))}
+            <form>
+                <input type="text" placeholder='calculate...' />
+                <div className='grid grid-cols-3'>
+                    {arr.map((data, idx) => (
+                        <Button key={idx} variant='secondary'>
+                            <span>{data}</span>
+                        </Button>
+                    ))}
+                </div>
             </form>
         </Card>
     )
