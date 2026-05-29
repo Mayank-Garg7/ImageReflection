6753eb55871bfc3d6da73891b058e8e67b623cdb
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-05-29 20:37:22 +0530

    update the form

--- src/features/calculator/Calculator.jsx
+++ src/features/calculator/Calculator.jsx
@@ -11,7 +11,7 @@ function Calculator() {
                     <input 
                     type="text" 
                     placeholder='calculate...' 
-                    className="w-full rounded-lg border border-slate-300 bg-white px-4 py-3 text-sm text-slate-700 shadow-sm outline-none transition duration-200 placeholder:text-slate-400 focus:border-blue-500 focus:ring-4 focus:ring-blue-100"
+                    className="w-full rounded-lg border px-4 py-3  text-slate-700  outline-none transition duration-200 placeholder:text-slate-400 focus:border-blue-500 focus:ring-4 focus:ring-blue-100"
                     />
                     <div className='grid grid-cols-3'>
                         {arr.map((data, idx) => (
